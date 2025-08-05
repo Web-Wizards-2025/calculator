f993d8145134d9706976a565380aa831b29cae63
mari-avp <marianvelezportillo@gmail.com>
2025-08-05 14:20:13 -0400

    operate functionfeatv:morel

--- index.js
+++ index.js
@@ -32,3 +32,17 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+function operate(operator, a, b) {
+    switch (operator) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case '*':
+        return multiply(a, b);
+      case '/':
+        return divide(a, b);
+      default:
+        return "Invalid operator";
+    }
+}
