0cc5e12b592e9ea7f60305d6acef1b134cd655ff
mari-avp <80354695+mari-avp@users.noreply.github.com>
2025-08-06 17:51:48 -0400

    operate function (#10)
    * operate function feat: rebase
    
    * operate functionfeatv:morel
    
    * operate function feat: rebase
    
    * operate function feat: rebase

--- index.js
+++ index.js
@@ -31,3 +31,18 @@ document.addEventListener("DOMContentLoaded", () => {
     setTheme(isDark);
   });
 });
+
+function operate(operator, a, b) {
+    switch (operator) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case '*':
+        return multiply(a, b);
+      case '/':
+        return divide(a, b);
+      default:
+        return "Invalid operator";
+    }
+  }
